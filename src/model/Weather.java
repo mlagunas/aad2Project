fa8d00fa3d7b04b26983418e78c1b905b723bc0f
toscan <toscan.vertanessian@gmail.com>
2014-10-08 14:02:33 +0200

    Weather and Plant class

---
+++ src/model/Weather.java
@@ -0,0 +1,98 @@
+package model;
+
+public class Weather {
+
+	
+	
+	private int maxHumi;
+	private int minHumi;
+	private int maxTemp;
+	private int minTemp;
+	private int minLightness;
+	private int maxLightnesss;
+	
+	private int id;
+
+public Weather(){}
+ 
+	
+	public int getId() {
+		return id;
+	}
+ 
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	
+	public int getMaxHumi() {
+		return maxHumi;
+	}
+
+
+	public void setMaxHumi(int maxHumi) {
+		this.maxHumi = maxHumi;
+	}
+
+
+	public int getMinHumi() {
+		return minHumi;
+	}
+
+
+	public void setMinHumi(int minHumi) {
+		this.minHumi = minHumi;
+	}
+
+
+	public int getMaxTemp() {
+		return maxTemp;
+	}
+
+
+	public void setMaxTemp(int maxTemp) {
+		this.maxTemp = maxTemp;
+	}
+
+
+	public int getMinTemp() {
+		return minTemp;
+	}
+
+
+	public void setMinTemp(int minTemp) {
+		this.minTemp = minTemp;
+	}
+
+
+	public int getMinLightness() {
+		return minLightness;
+	}
+
+
+	public void setMinLightness(int minLightness) {
+		this.minLightness = minLightness;
+	}
+
+
+	public int getMaxLightnesss() {
+		return maxLightnesss;
+	}
+
+
+	public void setMaxLightnesss(int maxLightnesss) {
+		this.maxLightnesss = maxLightnesss;
+	}
+
+
+	
+ 
+	
+
+ 
+	 
+
+	
+	
+	
+}
--- src/model/plant.java
+++ src/model/plant.java
@@ -1,5 +1,39 @@
 package model;
 
-public class plant {
+public class Plant {
 
-}
+	private int id;
+	private String description;
+	private Weather weather;
+ 
+	public Plant(){}
+ 
+	
+ 
+	public int getId() {
+		return id;
+	}
+ 
+	public void setId(int id) {
+		this.id = id;
+	}
+ 
+	public String getDescription() {
+		return description;
+	}
+ 
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	public Weather getWeatherId()
+	{
+		return weather;
+	}
+	
+	public void setWeatherId(Weather weather)
+	{
+		this.weather = weather;
+	}
+ 
+	}
