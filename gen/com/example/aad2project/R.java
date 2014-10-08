b5bc87f9a5cd438ea48e328f06cd859228eda874
Liyra <alexandre.deneuve@outlook.com>
2014-10-08 11:16:41 +0200

    LoginScreen

--- gen/com/example/aad2project/R.java
+++ gen/com/example/aad2project/R.java
@@ -1091,10 +1091,11 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f05003c;
+        public static final int action_settings=0x7f050041;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
+        public static final int button1=0x7f05003f;
         public static final int checkbox=0x7f05002d;
         public static final int collapseActionView=0x7f05000d;
         public static final int default_activity_button=0x7f050028;
@@ -1102,9 +1103,11 @@ containing a value of this type.
         public static final int disableHome=0x7f050008;
         public static final int dropdown=0x7f05000f;
         public static final int edit_query=0x7f050030;
+        public static final int email=0x7f05003d;
         public static final int end=0x7f050013;
         public static final int expand_activities_button=0x7f050026;
         public static final int expanded_menu=0x7f05002c;
+        public static final int greenhub=0x7f05003c;
         public static final int home=0x7f050014;
         public static final int homeAsUp=0x7f050005;
         public static final int icon=0x7f05002a;
@@ -1114,8 +1117,10 @@ containing a value of this type.
         public static final int list_item=0x7f050029;
         public static final int middle=0x7f050012;
         public static final int never=0x7f050009;
+        public static final int newAccount=0x7f050040;
         public static final int none=0x7f050010;
         public static final int normal=0x7f050000;
+        public static final int password=0x7f05003e;
         public static final int progress_circular=0x7f050018;
         public static final int progress_horizontal=0x7f050019;
         public static final int radio=0x7f05002f;
@@ -1199,6 +1204,7 @@ containing a value of this type.
         public static final int main=0x7f0c0000;
     }
     public static final class string {
+        public static final int Login=0x7f0a0012;
         /**  Content description for the action bar "home" affordance. [CHAR LIMIT=NONE] 
          */
         public static final int abc_action_bar_home_description=0x7f0a0001;
@@ -1239,9 +1245,12 @@ containing a value of this type.
         /**  Description of a share target (both in the list of such or the default share button) in a ShareActionProvider (share UI). [CHAR LIMIT=NONE] 
          */
         public static final int abc_shareactionprovider_share_with_application=0x7f0a000b;
-        public static final int action_settings=0x7f0a000f;
+        public static final int action_settings=0x7f0a000e;
         public static final int app_name=0x7f0a000d;
-        public static final int hello_world=0x7f0a000e;
+        public static final int emailHint=0x7f0a000f;
+        public static final int greenhub=0x7f0a0011;
+        public static final int newAccount=0x7f0a0013;
+        public static final int passwordHint=0x7f0a0010;
     }
     public static final class style {
         /** 
--- src/com/example/aad2project/MainActivity.java
+++ src/com/example/aad2project/MainActivity.java
@@ -1,30 +1,25 @@
-package com.example.loginscreen;
+package com.example.aad2project;
 
 import android.app.Activity;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
    private EditText  username=null;
    private EditText  password=null;
-   private TextView attempts;
    private Button login;
-   int counter = 3;
+   
    @Override
    protected void onCreate(Bundle savedInstanceState) {
       super.onCreate(savedInstanceState);
       setContentView(R.layout.activity_main);
-      username = (EditText)findViewById(R.id.editText1);
-      password = (EditText)findViewById(R.id.editText2);
-      attempts = (TextView)findViewById(R.id.textView5);
-      attempts.setText(Integer.toString(counter));
+      username = (EditText)findViewById(R.id.email);
+      password = (EditText)findViewById(R.id.password);
       login = (Button)findViewById(R.id.button1);
    }
 
@@ -37,12 +32,6 @@ public class MainActivity extends Activity {
    else{
       Toast.makeText(getApplicationContext(), "Wrong Credentials",
       Toast.LENGTH_SHORT).show();
-      attempts.setBackgroundColor(Color.RED);	
-      counter--;
-      attempts.setText(Integer.toString(counter));
-      if(counter==0){
-         login.setEnabled(false);
-      }
 
    }
 
