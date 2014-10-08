1d0d8e70ccde728c51d225cf290063a0e5db001e
hantzen <kevin.hantzen@gmail.com>
2014-10-08 17:10:14 +0200

    Add database handler

--- gen/com/example/aad2project/R.java
+++ gen/com/example/aad2project/R.java
@@ -1078,8 +1078,10 @@ containing a value of this type.
         public static final int abc_textfield_searchview_right_holo_dark=0x7f020055;
         public static final int abc_textfield_searchview_right_holo_light=0x7f020056;
         public static final int ic_launcher=0x7f020057;
+        public static final int testimagegs=0x7f020058;
     }
     public static final class id {
+        public static final int accountCreation=0x7f050042;
         public static final int action_bar=0x7f05001c;
         public static final int action_bar_activity_content=0x7f050015;
         public static final int action_bar_container=0x7f05001b;
@@ -1091,19 +1093,21 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f050041;
+        public static final int action_settings=0x7f050046;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
         public static final int button1=0x7f05003f;
         public static final int checkbox=0x7f05002d;
         public static final int collapseActionView=0x7f05000d;
+        public static final int confimrpasswordCreationAccount=0x7f050045;
         public static final int default_activity_button=0x7f050028;
         public static final int dialog=0x7f05000e;
         public static final int disableHome=0x7f050008;
         public static final int dropdown=0x7f05000f;
         public static final int edit_query=0x7f050030;
         public static final int email=0x7f05003d;
+        public static final int emailCreationAccount=0x7f050043;
         public static final int end=0x7f050013;
         public static final int expand_activities_button=0x7f050026;
         public static final int expanded_menu=0x7f05002c;
@@ -1120,7 +1124,9 @@ containing a value of this type.
         public static final int newAccount=0x7f050040;
         public static final int none=0x7f050010;
         public static final int normal=0x7f050000;
+        public static final int pager=0x7f050041;
         public static final int password=0x7f05003e;
+        public static final int passwordCreationAccount=0x7f050044;
         public static final int progress_circular=0x7f050018;
         public static final int progress_horizontal=0x7f050019;
         public static final int radio=0x7f05002f;
@@ -1198,13 +1204,17 @@ containing a value of this type.
         public static final int abc_search_view=0x7f030016;
         public static final int abc_simple_decor=0x7f030017;
         public static final int activity_main=0x7f030018;
-        public static final int support_simple_spinner_dropdown_item=0x7f030019;
+        public static final int activity_manager=0x7f030019;
+        public static final int fragment_account_creation=0x7f03001a;
+        public static final int fragment_plant_manager=0x7f03001b;
+        public static final int fragment_task_calendar=0x7f03001c;
+        public static final int support_simple_spinner_dropdown_item=0x7f03001d;
     }
     public static final class menu {
         public static final int main=0x7f0c0000;
+        public static final int manager=0x7f0c0001;
     }
     public static final class string {
-        public static final int Login=0x7f0a0012;
         /**  Content description for the action bar "home" affordance. [CHAR LIMIT=NONE] 
          */
         public static final int abc_action_bar_home_description=0x7f0a0001;
@@ -1245,12 +1255,22 @@ containing a value of this type.
         /**  Description of a share target (both in the list of such or the default share button) in a ShareActionProvider (share UI). [CHAR LIMIT=NONE] 
          */
         public static final int abc_shareactionprovider_share_with_application=0x7f0a000b;
+        public static final int accountCreationFragment=0x7f0a0016;
         public static final int action_settings=0x7f0a000e;
         public static final int app_name=0x7f0a000d;
+        public static final int confirmPasswordHint=0x7f0a0011;
+        public static final int create=0x7f0a0014;
         public static final int emailHint=0x7f0a000f;
-        public static final int greenhub=0x7f0a0011;
-        public static final int newAccount=0x7f0a0013;
+        public static final int greenhub=0x7f0a0012;
+        public static final int hello_blank_fragment=0x7f0a001c;
+        public static final int hello_world=0x7f0a001b;
+        public static final int login=0x7f0a0013;
+        public static final int newAccount=0x7f0a0015;
         public static final int passwordHint=0x7f0a0010;
+        public static final int title_activity_manager=0x7f0a0017;
+        public static final int title_section1=0x7f0a0018;
+        public static final int title_section2=0x7f0a0019;
+        public static final int title_section3=0x7f0a001a;
     }
     public static final class style {
         /** 
