a69d214f5d5900f971b3ce82b40bd1915b86741d
GSala <gsala24@gmail.com>
2014-10-08 14:58:24 +0200

    Two main fragments created

---
+++ src/com/example/aad2project/PlantManagerFragment.java
@@ -0,0 +1,28 @@
+package com.example.aad2project;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * 
+ */
+public class PlantManagerFragment extends Fragment {
+
+	public PlantManagerFragment() {
+		// Required empty public constructor
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		// Inflate the layout for this fragment
+		View view = inflater.inflate(R.layout.fragment_plant_manager,
+				container, false);
+		return view;
+	}
+
+}
