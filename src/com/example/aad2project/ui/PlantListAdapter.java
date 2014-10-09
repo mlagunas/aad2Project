e05d8688e3889af44349be93e92f254913d615aa
GSala <gsala24@gmail.com>
2014-10-09 17:30:22 +0200

    Improved Plants ListView

---
+++ src/com/example/aad2project/ui/PlantListAdapter.java
@@ -0,0 +1,128 @@
+package com.example.aad2project.ui;
+
+import java.util.List;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.TextView;
+
+import com.example.aad2project.R;
+import com.example.aad2project.model.Plant;
+
+public class PlantListAdapter extends BaseExpandableListAdapter {
+
+	private List<Plant> added;
+	private List<Plant> all;
+	private Context context;
+
+	public PlantListAdapter(Context context, List<Plant> added, List<Plant> all) {
+		this.context = context;
+		this.added = added;
+		this.all = all;
+	}
+
+	@Override
+	public int getGroupCount() {
+		return 2;
+	}
+
+	@Override
+	public int getChildrenCount(int groupPosition) {
+		switch (groupPosition) {
+		case 0:
+			return added.size();
+		case 1:
+			return all.size();
+		}
+		return 0;
+	}
+
+	@Override
+	public Object getGroup(int groupPosition) {
+		switch (groupPosition) {
+		case 0:
+			return "Your Plants";
+		case 1:
+			return "All Plants";
+		}
+		return null;
+	}
+
+	@Override
+	public Object getChild(int groupPosition, int childPosition) {
+		switch (groupPosition) {
+		case 0:
+			return added.get(childPosition);
+		case 1:
+			return all.get(childPosition);
+		}
+		return null;
+	}
+
+	@Override
+	public long getGroupId(int groupPosition) {
+		return groupPosition;
+	}
+
+	@Override
+	public long getChildId(int groupPosition, int childPosition) {
+		return childPosition;
+	}
+
+	@Override
+	public boolean hasStableIds() {
+		return true;
+	}
+
+	@Override
+	public View getGroupView(int groupPosition, boolean isExpanded,
+			View convertView, ViewGroup parent) {
+		if (convertView == null) {
+			LayoutInflater infalInflater = (LayoutInflater) context
+					.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+			convertView = infalInflater.inflate(
+					R.layout.plant_row_layout_group, null);
+		}
+
+		TextView groupTitle = (TextView) convertView
+				.findViewById(R.id.group_title);
+
+		groupTitle.setText((String) getGroup(groupPosition));
+
+		return convertView;
+	}
+
+	@Override
+	public View getChildView(int groupPosition, int childPosition,
+			boolean isLastChild, View convertView, ViewGroup parent) {
+
+		if (convertView == null) {
+			LayoutInflater infalInflater = (LayoutInflater) context
+					.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+			convertView = infalInflater
+					.inflate(R.layout.plant_row_layout, null);
+		}
+
+		TextView plantName = (TextView) convertView
+				.findViewById(R.id.plant_name);
+		Plant plant = null;
+		switch (groupPosition) {
+		case 0:
+			plant = added.get(childPosition);
+		case 1:
+			plant = all.get(childPosition);
+		}
+
+		plantName.setText(plant.getName());
+
+		return convertView;
+	}
+
+	@Override
+	public boolean isChildSelectable(int groupPosition, int childPosition) {
+		return true;
+	}
+}
--- src/com/example/aad2project/ui/PlantManagerFragment.java
+++ src/com/example/aad2project/ui/PlantManagerFragment.java
@@ -5,7 +5,6 @@ import java.util.ArrayList;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.ListFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,6 +12,7 @@ import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.AdapterView.OnItemLongClickListener;
 import android.widget.ArrayAdapter;
+import android.widget.ExpandableListView;
 import android.widget.Toast;
 
 import com.example.aad2project.R;
@@ -22,81 +22,118 @@ import com.example.aad2project.model.Plant;
  * A simple {@link Fragment} subclass.
  * 
  */
-public class PlantManagerFragment extends ListFragment {
-	
+public class PlantManagerFragment extends Fragment {
+
 	ArrayList<String> data;
 	ArrayAdapter<String> lvAdapter;
+	
+	private ExpandableListView list;
 
 	public PlantManagerFragment() {
 		// Required empty public constructor
 	}
+
 	/**
 	 * This method return all the data stored in the Database regarding to the
 	 * user who is using the application
+	 * 
 	 * @return
 	 */
-	private ArrayList<String> getPlants(){
-		//Need the data from the database to initialize the Array
+	private ArrayList<String> getStringPlants() {
+		// Need the data from the database to initialize the Array
 		// So we invent various plant objects and their parameters
 		ArrayList<String> plants = new ArrayList<String>();
-		Plant p1 = new Plant(); p1.setName("potatoes");
-		Plant p2 = new Plant(); p2.setName("tomatoes");
-		Plant p3 = new Plant(); p3.setName("onions");
-		Plant p4 = new Plant(); p4.setName("garlics");
-		plants.add(p1.getName());plants.add(p2.getName());
-		plants.add(p3.getName());plants.add(p4.getName());
+		Plant p1 = new Plant();
+		p1.setName("potatoes");
+		Plant p2 = new Plant();
+		p2.setName("tomatoes");
+		Plant p3 = new Plant();
+		p3.setName("onions");
+		Plant p4 = new Plant();
+		p4.setName("garlics");
+		plants.add(p1.getName());
+		plants.add(p2.getName());
+		plants.add(p3.getName());
+		plants.add(p4.getName());
 		return plants;
 	}
-	
+
+	private ArrayList<Plant> getPlants() {
+		// Need the data from the database to initialize the Array
+		// So we invent various plant objects and their parameters
+		ArrayList<Plant> plants = new ArrayList<Plant>();
+		Plant p1 = new Plant();
+		p1.setName("potatoes");
+		Plant p2 = new Plant();
+		p2.setName("tomatoes");
+		Plant p3 = new Plant();
+		p3.setName("onions");
+		Plant p4 = new Plant();
+		p4.setName("garlics");
+		plants.add(p1);
+		plants.add(p2);
+		plants.add(p3);
+		plants.add(p4);
+		return plants;
+	}
+
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		// Inflate the layout for this fragment
 		View view = inflater.inflate(R.layout.fragment_plant_manager,
 				container, false);
-		lvAdapter = new ArrayAdapter<String>(getActivity(),
-				android.R.layout.simple_list_item_1, getPlants());
-		setListAdapter(lvAdapter);
+		
+		list = (ExpandableListView) view.findViewById(R.id.list);
+		
+		/*lvAdapter = new ArrayAdapter<String>(getActivity(),
+				android.R.layout.simple_list_item_1, getStringPlants());*/
+		
+		PlantListAdapter adapter = new PlantListAdapter(getActivity(), getPlants(), getPlants());
+		
+		list.setAdapter(adapter);
+		list.expandGroup(0);
 		return view;
 	}
-	
+
 	@Override
 	public void onActivityCreated(Bundle savedState) {
-	    super.onActivityCreated(savedState);
+		super.onActivityCreated(savedState);
 
-	    getListView().setOnItemClickListener(new OnItemClickListener() {
+		list.setOnItemClickListener(new OnItemClickListener() {
 
 			@Override
 			public void onItemClick(AdapterView<?> parent, View view,
 					int position, long id) {
-				// TODO Auto-generated method stub
-				Toast.makeText(getActivity(), "Short click", Toast.LENGTH_SHORT).show();
-				
-			}	
-		});
-	    
-	    getListView().setOnItemLongClickListener(new OnItemLongClickListener() {
+				Toast.makeText(getActivity(), "Short click", Toast.LENGTH_SHORT)
+						.show();
 
-	        @Override
-	        public boolean onItemLongClick(AdapterView<?> arg0, View arg1,
-	                int arg2, long arg3) {
-	        			
-	        			// Create dialog
-	        			DialogFragment dialog = new LongClickDialogFragment();
-	        			
-	        			// Put boolean to show Add or Delete
-	        			Bundle bundle = new Bundle();
-	        			bundle.putBoolean("add", true); // TODO Put true or false if the plant is added or not
-	        			dialog.setArguments(bundle);
-	        			
-	        			// Show dialog
-	        			dialog.show(getFragmentManager(), "longClickDialog");
-	        			
-	        			
-			        	Toast.makeText(getActivity(), "On long click listener", Toast.LENGTH_LONG).show();
-			            return true;
-			            
-	        }
-	    });
+			}
+		});
+
+		list.setOnItemLongClickListener(new OnItemLongClickListener() {
+
+			@Override
+			public boolean onItemLongClick(AdapterView<?> parent, View view,
+					int position, long id) {
+
+				// Create dialog
+				DialogFragment dialog = new LongClickDialogFragment();
+
+				// Put boolean to show Add or Delete
+				Bundle bundle = new Bundle();
+				bundle.putBoolean("add", true); // TODO Put true or false if the
+												// plant is added or not
+				dialog.setArguments(bundle);
+
+				// Show dialog
+				dialog.show(getFragmentManager(), "longClickDialog");
+
+				Toast.makeText(getActivity(), "On long click listener",
+						Toast.LENGTH_LONG).show();
+				return true;
+
+			}
+		});
 	}
 }
