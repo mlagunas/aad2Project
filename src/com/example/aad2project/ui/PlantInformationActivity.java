82869d3f804807d64d71bbcde777e7340dde9a33
Manuel Lagunas <mlagunas94@gmail.com>
2014-10-11 15:18:23 +0200

    Plant Information Activity added

---
+++ src/com/example/aad2project/ui/PlantInformationActivity.java
@@ -0,0 +1,18 @@
+package com.example.aad2project.ui;
+
+import com.example.aad2project.R;
+import com.example.aad2project.R.layout;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class PlantInformationActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_plant_information);
+	}
+}
