08e0e5276efe7582b74e59f782356bf068c26d82
hantzen <kevin.hantzen@gmail.com>
2014-10-12 15:17:22 +0200

    fix error with gitIgnore

---
+++ src/com/example/aad2project/model/DaoBase.java
@@ -0,0 +1,39 @@
+package com.example.aad2project.model;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+public abstract class DaoBase {
+	  // First base version
+	  // Update this attribute if version change
+	  protected final static int VERSION = 1;
+	  // database name
+	  protected final static String NOM = "database.db";
+	  
+	  protected SQLiteDatabase mDb = null;
+	  protected DatabaseHandler mHandler = null;
+
+	  protected String query;
+	  protected String[] args;
+	  protected Cursor c;
+  
+	  public DaoBase(Context pContext) {
+		    this.mHandler = new DatabaseHandler(pContext, NOM, null, VERSION);
+		  }
+		    
+	  public SQLiteDatabase open() {
+	    mDb = mHandler.getWritableDatabase();
+	    return mDb;
+	  }
+	    
+	  public void close() {
+	    mDb.close();
+	  }
+	    
+	  public SQLiteDatabase getDb() {
+	    return mDb;
+	  }
+     
+	  protected abstract boolean convertResultToObject (Cursor c);
+}
