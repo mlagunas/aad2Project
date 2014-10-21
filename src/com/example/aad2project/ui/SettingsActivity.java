67483900b104c8912dbe98e2aef81a4d0a7745ba
Asier <mexgraflo@gmail.com>
2014-10-21 16:26:06 +0200

    TODO updated

--- src/com/example/aad2project/ui/SettingsActivity.java
+++ src/com/example/aad2project/ui/SettingsActivity.java
@@ -1,20 +1,21 @@
 package com.example.aad2project.ui;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
-import android.widget.ArrayAdapter;
+import android.preference.PreferenceManager;
 
 import com.example.aad2project.R;
 
 public class SettingsActivity extends PreferenceActivity {
-
-	private ArrayAdapter<String> adapter;
+	
+	private SharedPreferences sharedPrefs ;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		addPreferencesFromResource(R.xml.preferences);
-		
-		
+		sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+		String syncConnPref = sharedPrefs.getString("pref_time", "");
 	}
 }
