d55ac15a5248123c102159160574ecb082e128b6
Manuel Lagunas <mlagunas94@gmail.com>
2014-10-26 16:14:35 +0100

    Get weather

--- src/com/example/aad2project/model/WeatherDao.java
+++ src/com/example/aad2project/model/WeatherDao.java
@@ -8,5 +8,22 @@ public class WeatherDao extends DaoBase {
 		super(pContext);
 	}
 	
+	public Weather getWeather(int id){
+		c = super.mDb.rawQuery("SELECT * FROM Weather WHERE id = " + id, null);
+		if(c.moveToFirst()){
+			Weather w = new Weather();
+			w.setId(c.getInt(0));
+			w.setMinTemp(c.getInt(1));
+			w.setMaxTemp(c.getInt(2));
+			w.setMinHumi(c.getInt(3));
+			w.setMaxHumi(c.getInt(4));
+			w.setMinLightness(c.getInt(5));
+			w.setMinLightness(c.getInt(6));
+			return w;
+		}
+		else
+			return null;
+	}
+	
 	protected boolean convertResultToObject() { return true;}
 }
